cf648cebe8b32ab8b3e393c2f31e078df82fdea4
Adam <w.adamo@outlook.com>
2025-01-16 18:05:36 +0100

    Pierwsze kroki

---
+++ io_p1/src/test/java/PresenterTest.java
@@ -0,0 +1,41 @@
+public void editRoute(String data) {
+    Role role = currentUser.getRole();
+    if(role == Role.ITemployee){
+        ArrayList<String> dataArray = new ArrayList<String>(Arrays.asList(data.split(",")));
+        String operation;
+        int lineID,routeID;
+        if (dataArray.size() >= 2) {
+            operation = dataArray.get(0);
+            dataArray.remove(0);
+            lineID = Integer.parseInt(dataArray.get(0));
+            dataArray.remove(0);
+            routeID = Integer.parseInt(dataArray.get(0));
+            dataArray.remove(0);
+            if(operation == "delete"){
+                dataUpdater.deleteRoute(routeID,lineID);
+                return;
+            }
+            Route route = new Route();
+            route.parseRoute(dataArray);
+            if(operation == "add"){
+                dataUpdater.addRoute(route,lineID);
+            }else if(operation == "change"){
+                dataUpdater.updateRoute(route,lineID,lineID);
+            }
+        }
+    }
+    //TODO - Kody błędu
+}
+
+
+public class PresenterTest {
+    User currentUser
+
+    @Test
+    void testEditRoute(){
+        when(CurrentUser.getRole()).thenReturn(Role.ItEmployee);
+
+        ArrayList<String> dataArray = new ArrayList<String>("")
+
+    }
+}
